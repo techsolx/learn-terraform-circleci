509550e191bed277f4bce6cce6b5e89ce87ef37f
John MacTavish <john.mactavish@evercast.us>
2021-01-27 21:06:03 -0800

    Fix backend location, fmt.

--- main.tf
+++ main.tf
@@ -4,13 +4,13 @@ terraform {
       source = "hashicorp/aws"
     }
   }
+  backend "s3" {
+    bucket = "8af3b125-9907-9d0b-7e1d-f7f6f79436d2-backend"
+    key    = "terraform/webapp/terraform.tfstate"
+    region = "us-west-2"
+  }
 }
 
-backend "s3" {
-  bucket = "8af3b125-9907-9d0b-7e1d-f7f6f79436d2-backend"
-  key = "terraform/webapp/terraform.tfstate"
-  region = "us-west-2"
-}
 
 provider "aws" {
   region = var.region
--- variables.tfvars
+++ variables.tfvars
@@ -1,4 +1,4 @@
 region = "us-west-2"
-user = "ci-cd-deploy-user"
-label = "hashicorp.fun"
-app = "terramino"
+user   = "ci-cd-deploy-user"
+label  = "hashicorp.fun"
+app    = "terramino"
